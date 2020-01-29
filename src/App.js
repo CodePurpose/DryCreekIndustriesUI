8f0387ca5f047d269aded26af9276d56d615e9ee
Emilio <em.francomartinez@gmail.com>
2020-01-28 20:27:33 -0700

    convert NavBar to functional compoenent. Passed title and links to NavBar as props form app.js.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,31 @@ import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./components/navbar";
 
+const title = { name: "DRY CREEK INDUSTRIES", subTitle: "General Contracting" };
+
+const links = [
+  {
+    name: "Home",
+    path: "/"
+  },
+  {
+    name: "Testimonials",
+    path: "/"
+  },
+  {
+    name: "Gallery",
+    path: "/"
+  },
+  {
+    name: "Contact Us",
+    path: "/"
+  }
+];
+
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <NavBar links={links} title={title} />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello World!</p>
--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,47 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 import { TitleContent, HeaderContent, Title, SubTitle, Nav } from "./Styles";
 
-export default class NavBar extends Component {
-  //add state for controlling mobile nav
-  render() {
-    return (
-      <header>
-        <HeaderContent>
-          <TitleContent>
-            <Title>{title.name}</Title>
-            <hr />
-            <SubTitle>{title.subTitle}</SubTitle>
-          </TitleContent>
-          <Nav>
-            {links.map((link, index) => (
-              <li key={index}>
-                <a href={link.path}>{link.name}</a>
-              </li>
-            ))}
-          </Nav>
-        </HeaderContent>
-      </header>
-    );
-  }
-}
+const NavBar = props => {
+  const { title, links } = props;
+  return (
+    <header>
+      <HeaderContent>
+        <TitleContent>
+          <Title>{title.name}</Title>
+          <hr />
+          <SubTitle>{title.subTitle}</SubTitle>
+        </TitleContent>
+        <Nav>
+          {links.map((link, index) => (
+            <li key={index}>
+              <a href={link.path}>{link.name}</a>
+            </li>
+          ))}
+        </Nav>
+      </HeaderContent>
+    </header>
+  );
+};
 
-const title = { name: "DRY CREEK INDUSTRIES", subTitle: "General Contracting" };
-
-const links = [
-  {
-    name: "Home",
-    path: "/"
-  },
-  {
-    name: "Testimonials",
-    path: "/"
-  },
-  {
-    name: "Gallery",
-    path: "/"
-  },
-  {
-    name: "Contact Us",
-    path: "/"
-  }
-];
+export default NavBar;
