5ac01f98572c454322954728447b1c733975c392
Emilio <em.francomartinez@gmail.com>
2020-02-13 15:02:29 -0700

    cropped images to square ratio so they work in hexagon

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+
 import NavBar from "./components/navbar";
+import HexGallery from "./components/hexGallery";
+
+import cabinets from "./images/cabinets.jpg";
+import carpentryFinish from "./images/carpentryFinishWork.jpeg";
+import flooring from "./images/flooring.jpg";
+import homeMaintenace from "./images/homeMaintenance.jpg";
+import homeRemodeling from "./images/homeRemodeling.jpg";
+import lighting from "./images/lighting.jpg";
+import railing from "./images/railing.jpg";
+import roughCarpentry from "./images/roughCarpentry.jpg";
+import shelving from "./images/shelving.jpg";
+import stairCases from "./images/stairCases.jpg";
 
 const title = { name: "DRY CREEK INDUSTRIES", subTitle: "General Contracting" };
 
 const links = [
-  {
-    name: "Home",
-    path: "/"
-  },
-  {
-    name: "Testimonials",
-    path: "/"
-  },
-  {
-    name: "Gallery",
-    path: "/"
-  },
-  {
-    name: "Contact Us",
-    path: "/"
-  }
+  { name: "Home", path: "/" },
+  { name: "Testimonials", path: "/" },
+  { name: "Gallery", path: "/" },
+  { name: "Contact Us", path: "/" }
+];
+
+const rowOne = [
+  { title: "Rough Carpentry", img: roughCarpentry },
+  { title: "Carpentry Finish Work", img: carpentryFinish },
+  { title: "Home Remodeling", img: homeRemodeling },
+  { title: "Home Maintence", img: homeMaintenace }
+];
+const rowTwo = [
+  { title: "Cabinets", img: cabinets },
+  { title: "Lighting", img: lighting },
+  { title: "Railing", img: railing }
+];
+const rowThree = [
+  { title: "Stair Cases", img: stairCases },
+  { title: "Flooring", img: flooring },
+  { title: "Shelving", img: shelving },
+  { title: "Concrete", img: cabinets }
 ];
 
 function App() {
@@ -32,6 +51,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello World!</p>
       </header>
+      <HexGallery rowOne={rowOne} rowTwo={rowTwo} rowThree={rowThree} />
     </div>
   );
 }
