5484b1c4a2410b284f75e69379679b4e6c462998
Emilio <em.francomartinez@gmail.com>
2020-02-16 17:17:24 -0700

    update styles and create seprate function to render items

--- src/components/aboutUs/Styles.js
+++ src/components/aboutUs/Styles.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 const mobile = "480px";
+const smallDeskTop = "1024px";
 
 export const AboutUsBackground = styled.div`
   background-color: ${props => props.color};
@@ -16,27 +17,37 @@ export const AboutUsContainer = styled.div`
   flex-wrap: wrap;
 `;
 
+export const ItemContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
 export const TextContainer = styled.div`
   max-width: 470px;
 
   > h3 {
     margin: 0;
   }
-`;
 
-export const ItemContainer = styled.div`
-  display: flex;
-  align-items: center;
+  @media screen and (max-width: ${smallDeskTop}) {
+    max-width: 370px;
+  }
 `;
 
 export const IconContainer = styled.i`
-  font-size: 3em;
+  font-size: 3rem;
   margin-right: 40px;
   width: 65px;
 
+  @media screen and (max-width: ${smallDeskTop}) {
+    font-size: 2rem;
+    margin-right: 10px;
+    width: 55px;
+  }
+
   @media screen and (max-width: ${mobile}) {
-    font-size: 2em;
-    margin-right: 20px;
-    width: 60px;
+    font-size: 1.5rem;
+    margin-right: 10px;
+    width: 50px;
   }
 `;
--- src/components/aboutUs/index.js
+++ src/components/aboutUs/index.js
@@ -6,25 +6,30 @@ import {
   IconContainer,
   TextContainer
 } from "./Styles";
+
+const items = (item, index) => {
+  return (
+    <React.Fragment>
+      <ItemContainer key={index}>
+        <IconContainer>
+          <i className={item.icon} />
+        </IconContainer>
+        <TextContainer>
+          <h3>{item.title}</h3>
+          <p>{item.text}</p>
+        </TextContainer>
+      </ItemContainer>
+    </React.Fragment>
+  );
+};
+
 const AboutUs = props => {
   const { content, grey } = props;
 
   return (
     <AboutUsBackground color={grey}>
       <AboutUsContainer>
-        {content.map((item, index) => {
-          return (
-            <ItemContainer key={index}>
-              <IconContainer>
-                <i className={item.icon} />
-              </IconContainer>
-              <TextContainer>
-                <h3>{item.title}</h3>
-                <p>{item.text}</p>
-              </TextContainer>
-            </ItemContainer>
-          );
-        })}
+        {content.map((item, index) => items(item, index))}
       </AboutUsContainer>
     </AboutUsBackground>
   );
